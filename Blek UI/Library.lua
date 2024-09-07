fad096ed4bd60eaafc8ca52a109e6f696fd0f71e
RileyBeeRBLX4 <rileybeerblx4@gmail.com>
2024-09-07 10:21:15 +0800

    Update Library.lua

--- Blek UI/Library.lua
+++ Blek UI/Library.lua
@@ -498,41 +498,41 @@ function Library:Create(table)
 
         return ElementHandler
     end
+    
     --// Drag - not by me
-local frame = script.Parent.main
-local dragging = false
-local dragInput
-local dragStart
-local startPos
+local draggingEnabled = false
+local dragStartPos = nil
+local frameStartPos = nil
 
-local function update(input)
-    local delta = input.Position - dragStart
-    frame.Position = UDim2.new(startPos.X.Scale, startPos.X.Offset + delta.X, startPos.Y.Scale, startPos.Y.Offset + delta.Y)
+local function updatePosition(input)
+    local delta = input.Position - dragStartPos
+    Frame.Position = UDim2.new(
+        frameStartPos.X.Scale,
+        frameStartPos.X.Offset + delta.X,
+        frameStartPos.Y.Scale,
+        frameStartPos.Y.Offset + delta.Y
+    )
+    main.Position = Frame.Position
 end
 
 main.InputBegan:Connect(function(input)
     if input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch then
-        dragging = true
-        dragStart = input.Position
-        startPos = frame.Position
+        draggingEnabled = true
+        dragStartPos = input.Position
+        frameStartPos = Frame.Position
+    end
+end)
 
-        input.Changed:Connect(function()
-            if input.UserInputState == Enum.UserInputState.End then
-                dragging = false
-            end
-        end)
+main.InputEnded:Connect(function(input)
+    if input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch then
+        draggingEnabled = false
     end
 end)
 
 main.InputChanged:Connect(function(input)
-    if input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch then
-        dragInput = input
-    end
-end)
-
-game:GetService("UserInputService").InputChanged:Connect(function(input)
-    if dragging and input == dragInput then
-        update(input)
+    if draggingEnabled and
+        (input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch) then
+        updatePosition(input)
     end
 end)
 
