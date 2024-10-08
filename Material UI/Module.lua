ace1ad03aed9a715b31e70b5b8d93d4e1b99fd0b
RileyBeeRBLX4 <rileybeerblx4@gmail.com>
2024-10-08 23:50:23 +0800

    Update Module.lua

--- Material UI/Module.lua
+++ Material UI/Module.lua
@@ -792,50 +792,47 @@ function Material.Load(Config)
 	TitleText.Font = Enum.Font.GothamBold
 	TitleText.Parent = TitleBar
 
-TitleText.MouseButton1Down:Connect(function()
-    local UIS = game:GetService('UserInputService')
-    local TweenService = game:GetService('TweenService')
-    local dragToggle = false
-    local dragSpeed = 0.25
-    local dragStart = nil
-    local startPos = nil
+local frame = Objects.new("Round")
+frame.Parent = script.Parent
 
-    local frame = Objects.new("Round")
-    frame.Parent = script.Parent
+local function AddDraggingFunctionality(DragPoint, Main)
+    pcall(function()
+        local Dragging, DragInput, MousePos, FramePos = false
+        DragPoint.InputBegan:Connect(function(Input)
+            if Input.UserInputType == Enum.UserInputType.MouseButton1 or Input.UserInputType == Enum.UserInputType.Touch then
+                Dragging = true
+                MousePos = Input.Position
+                FramePos = Main.Position
 
-    local function updateInput(input)
-        local delta = input.Position - dragStart
-        local position = UDim2.new(startPos.X.Scale, startPos.X.Offset + delta.X,
-            startPos.Y.Scale, startPos.Y.Offset + delta.Y)
-        TweenService:Create(frame, TweenInfo.new(dragSpeed), {Position = position}):Play()
-    end
-
-    local function onInputChanged(input)
-        if (input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch) then
-            if dragToggle then
-                updateInput(input)
+                Input.Changed:Connect(function()
+                    if Input.UserInputState == Enum.UserInputState.End then
+                        Dragging = false
+                    end
+                end)
             end
-        end
-    end
+        end)
 
-    frame.InputBegan:Connect(function(input)
-        if (input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch) then
-            dragToggle = true
-            dragStart = input.Position
-            startPos = frame.Position
-            
-            input.Changed:Connect(function()
-                if input.UserInputState == Enum.UserInputState.End then
-                    dragToggle = false
-                end
-            end)
-        end
+        DragPoint.InputChanged:Connect(function(Input)
+            if Input.UserInputType == Enum.UserInputType.MouseMovement or Input.UserInputType == Enum.UserInputType.Touch then
+                DragInput = Input
+            end
+        end)
+
+        UserInputService.InputChanged:Connect(function(Input)
+            if Input == DragInput and Dragging then
+                local Delta = Input.Position - MousePos
+                TweenService:Create(Main, TweenInfo.new(0.45, Enum.EasingStyle.Quint, Enum.EasingDirection.Out), {
+                    Position = UDim2.new(FramePos.X.Scale, FramePos.X.Offset + Delta.X, FramePos.Y.Scale, FramePos.Y.Offset + Delta.Y)
+                }):Play()
+            end
+        end)
     end)
+end
 
-    UIS.InputChanged:Connect(onInputChanged)
+TitleText.MouseButton1Down:Connect(function()
+    AddDraggingFunctionality(TitleText, frame)
 end)
-
-
+	
 	local MinimiseButton = Objects.new("SmoothButton")
 	MinimiseButton.Size = UDim2.fromOffset(20,20)
 	MinimiseButton.Position = UDim2.fromScale(1,0) + UDim2.fromOffset(-25,5)
